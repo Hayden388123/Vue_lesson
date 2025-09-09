3617bdd1a44781ad4dacb0bc27233ba90573605d
Hayden388123 <hayden388123@gmail.com>
2025-09-09 10:39:40 +0800

    get網址中的data，並使用v-for迴圈

--- my_notes/src/components/axios.vue
+++ my_notes/src/components/axios.vue
@@ -1,9 +1,28 @@
 <script setup>
-import {ref} from 'vue'
+import axios1 from 'axios';
+import {ref, onMounted} from 'vue';
+const posts = ref([]);
+
+onMounted(async()=>{
+    try{
+        const response = await axios1.get('https://jsonplaceholder.typicode.com/posts');
+        posts.value = response.data;
+        console.log(response.data);
+        
+    }
+    catch(error){
+        console.log(error);      
+    }
+})
 </script>
 
 <template>
-
+<ul class="post-list">
+    <li v-for="item in posts">
+        <p>id:{{ item.id }}</p>
+        <p>title:{{ item.title }}</p>
+    </li>
+</ul>
 </template>
 
 <style scoped>
